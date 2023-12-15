a871a9be8f7945f82de60d4abcf56873a6e2f523
alexey <privalov_a90@bk.ru>
2023-12-15 13:15:53 +0300

    добавил метод каторый выводит на экран получившийся массив

--- Program.cs
+++ Program.cs
@@ -12,3 +12,11 @@ void ArrayIF(string[] array1, string[] array2)
         }
     }
 }
+void PrArr(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
